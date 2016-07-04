4c5573835f477637fd2dcf15e25ef68022627ab7
Michael Hinstridge <rangeroffire@gmail.com>
2016-07-04 02:37:06 +0100

    [feature] fake cinema provider

---
+++ faker_cinema/cinema.py
@@ -0,0 +1,19 @@
+from faker.providers import BaseProvider
+
+
+class CinemaProvider(BaseProvider):
+    formats = (
+        '{{city}}',
+    )
+
+    def cinema(self, exhibitor=None):
+        """
+        :param exhibitor: The exhibitor to use
+        :return: Royal Kino Sydneyberg
+        """
+        pattern = self.random_element(self.formats)
+        if exhibitor is None:
+            pattern = '{{exhibitor}} %s' % pattern
+            return self.generator.parse(pattern)
+        else:
+            return '{0} {1}'.format(exhibitor, self.generator.parse(pattern))
