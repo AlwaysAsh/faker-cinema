5cff29232d17ea05df1df35a28d86414d5ea1742
Michael Hinstridge <rangeroffire@gmail.com>
2016-07-04 02:38:19 +0100

    [feature] update init to allow easier importing and support for import *

---
+++ faker_cinema/__init__.py
@@ -0,0 +1,7 @@
+from .exhibitor import ExhibitorProvider
+from .cinema import CinemaProvider
+from .screen import ScreenProvider
+
+
+__author__ = 'Michael Hinstridge'
+__all__ = ['ExhibitorProvider', 'CinemaProvider', 'ScreenProvider']
